654c9b3f36c0ee7e75ba22d22d928cbbaeee9d16
Florian Küpper <florian.kuepper@gmail.com>
2015-02-27 15:22:02 +0100

    Added Bus.

---
+++ src/de/uni_muenster/sopra2015/gruppe8/octobus/model/Bus.java
@@ -0,0 +1,98 @@
+package de.uni_muenster.sopra2015.gruppe8.octobus.model;
+
+import java.util.Date;
+
+/**
+ * Created by Florian on 27.02.2015.
+ */
+public class Bus
+{
+	private String licencePlate;
+	private int numberOfSeats;
+	private int standingRooms;
+	private String manufactuer;
+	private String model;
+	private Date nexInspectionDue;
+	private boolean articulatedBus;
+
+	public Bus(String licencePlate, int numberOfSeats, int standingRooms, String manufactuer, String model, Date nexInspectionDue, boolean articulatedBus)
+	{
+		this.licencePlate = licencePlate;
+		this.numberOfSeats = numberOfSeats;
+		this.standingRooms = standingRooms;
+		this.manufactuer = manufactuer;
+		this.model = model;
+		this.nexInspectionDue = nexInspectionDue;
+		this.articulatedBus = articulatedBus;
+	}
+
+	public String getLicencePlate()
+	{
+		return licencePlate;
+	}
+
+	public void setLicencePlate(String licencePlate)
+	{
+		this.licencePlate = licencePlate;
+	}
+
+	public int getNumberOfSeats()
+	{
+		return numberOfSeats;
+	}
+
+	public void setNumberOfSeats(int numberOfSeats)
+	{
+		this.numberOfSeats = numberOfSeats;
+	}
+
+	public int getStandingRooms()
+	{
+		return standingRooms;
+	}
+
+	public void setStandingRooms(int standingRooms)
+	{
+		this.standingRooms = standingRooms;
+	}
+
+	public String getManufactuer()
+	{
+		return manufactuer;
+	}
+
+	public void setManufactuer(String manufactuer)
+	{
+		this.manufactuer = manufactuer;
+	}
+
+	public String getModel()
+	{
+		return model;
+	}
+
+	public void setModel(String model)
+	{
+		this.model = model;
+	}
+
+	public Date getNexInspectionDue()
+	{
+		return nexInspectionDue;
+	}
+
+	public void setNexInspectionDue(Date nexInspectionDue)
+	{
+		this.nexInspectionDue = nexInspectionDue;
+	}
+
+	public boolean isArticulatedBus()
+	{
+		return articulatedBus;
+	}
+
+	public void setArticulatedBus(boolean articulatedBus)
+	{
+		this.articulatedBus = articulatedBus;
+	}
+}
