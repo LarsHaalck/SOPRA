a3b50f38b137597b8be0df0cf7323fe0289d1249
LHulk <lars.haalck@arcor.de>
2015-02-27 15:22:28 +0100

    added Role, Tour

---
+++ src/de/uni_muenster/sopra2015/gruppe8/octobus/model/Role.java
@@ -0,0 +1,13 @@
+package de.uni_muenster.sopra2015.gruppe8.octobus.model;
+
+/**
+ * Created by Lars on 27-Feb-15.
+ */
+public enum Role
+{
+	BUSDRIVER,
+	NETWORK_PLANNER,
+	TICKET_PLANNER,
+	HR_MANAGER,
+	SCHEDULE_MANAGER
+}
