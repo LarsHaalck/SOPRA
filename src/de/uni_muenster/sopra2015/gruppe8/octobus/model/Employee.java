93f3e49c582ded3c873980cb2a614480a973f938
Jonas Hilbert <j_hilb04@wwu.de>
2015-02-27 15:25:04 +0100

    Klasse Employee erstellt mit automatisch generiertem Konstruktor und gettern und settern

---
+++ src/de/uni_muenster/sopra2015/gruppe8/octobus/model/Employee.java
@@ -0,0 +1,162 @@
+package de.uni_muenster.sopra2015.gruppe8.octobus.model;
+
+import java.util.Date;
+
+/**
+ * Created by Jonas on 27.02.2015.
+ */
+public class Employee
+{
+    private String name;
+    private String firstName;
+    private String address;
+    private String zipCode;
+    private String city;
+    private Date birthDate;
+    private String phone;
+    private String mail;
+    private String username;
+    private String password;
+    private String salt;
+    private String note;
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public String getAddress()
+    {
+        return address;
+    }
+
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
+
+    public String getZipCode()
+    {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode)
+    {
+        this.zipCode = zipCode;
+    }
+
+    public String getCity()
+    {
+        return city;
+    }
+
+    public void setCity(String city)
+    {
+        this.city = city;
+    }
+
+    public Date getBirthDate()
+    {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate)
+    {
+        this.birthDate = birthDate;
+    }
+
+    public String getPhone()
+    {
+        return phone;
+    }
+
+    public void setPhone(String phone)
+    {
+        this.phone = phone;
+    }
+
+    public String getMail()
+    {
+        return mail;
+    }
+
+    public void setMail(String mail)
+    {
+        this.mail = mail;
+    }
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+
+    public String getSalt()
+    {
+        return salt;
+    }
+
+    public void setSalt(String salt)
+    {
+        this.salt = salt;
+    }
+
+    public String getNote()
+    {
+        return note;
+    }
+
+    public void setNote(String note)
+    {
+        this.note = note;
+    }
+
+    public Employee(String name, String firstName, String address, String zipCode, String city, Date birthDate,
+                    String phone, String mail, String username, String password, String salt, String note)
+    {
+        this.name = name;
+        this.firstName = firstName;
+        this.address = address;
+        this.zipCode = zipCode;
+
+        this.city = city;
+        this.birthDate = birthDate;
+        this.phone = phone;
+        this.mail = mail;
+        this.username = username;
+        this.password = password;
+        this.salt = salt;
+        this.note = note;
+    }
+
+
+}
