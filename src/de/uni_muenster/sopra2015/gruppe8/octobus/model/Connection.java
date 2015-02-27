28c321563d4b11bb55d3e845d236b10297bbfef1
Joshua <Joshua.Es@gmx.de>
2015-02-27 15:31:09 +0100

    Connection Klammern gefixt

--- src/de/uni_muenster/sopra2015/gruppe8/octobus/model/Connection.java
+++ src/de/uni_muenster/sopra2015/gruppe8/octobus/model/Connection.java
@@ -8,47 +8,56 @@ import java.util.LinkedList;
  */
 public class Connection
 {
-    private LinkedList<Quadruple<int,BusStop,Route,BusStop>> trips;
+    private LinkedList<Quadruple<int, BusStop, Route, BusStop>> trips;
     private int duration;
     private Date date;
     private int time;
 
-    public Connection(LinkedList<Quadruple<int, BusStop, Route, BusStop>> trips, int duration, Date date, int time) {
+    public Connection(LinkedList<Quadruple<int, BusStop, Route, BusStop>> trips, int duration, Date date, int time)
+    {
         this.trips = trips;
         this.duration = duration;
         this.date = date;
         this.time = time;
     }
 
-    public LinkedList<Quadruple<int, BusStop, Route, BusStop>> getTrips() {
+    public LinkedList<Quadruple<int, BusStop, Route, BusStop>> getTrips()
+    {
         return trips;
     }
 
-    public void setTrips(LinkedList<Quadruple<int, BusStop, Route, BusStop>> trips) {
+    public void setTrips(LinkedList<Quadruple<int, BusStop, Route, BusStop>> trips)
+    {
         this.trips = trips;
     }
 
-    public int getDuration() {
+    public int getDuration()
+    {
         return duration;
     }
 
-    public void setDuration(int duration) {
+    public void setDuration(int duration)
+    {
         this.duration = duration;
     }
 
-    public Date getDate() {
+    public Date getDate()
+    {
         return date;
     }
 
-    public void setDate(Date date) {
+    public void setDate(Date date)
+    {
         this.date = date;
     }
 
-    public int getTime() {
+    public int getTime()
+    {
         return time;
     }
 
-    public void setTime(int time) {
+    public void setTime(int time)
+    {
         this.time = time;
     }
 }
