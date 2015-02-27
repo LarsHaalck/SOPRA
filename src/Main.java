99723535c7ab91f478a631a7fee1a17b0f002ed9
Phil Steinhorst <phist91@gmail.com>
2015-02-27 14:44:39 +0100

    Täst

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,9 @@
  * Created by Lars on 27-Feb-15.
  * Last changes: 27-Feb-15 by Florian
  */
-public class Main
-{
-    public static void main(String[] args)
-    {
+public class Main {
+    public static void main(String[] args) {
         System.out.println("Hallo");
+        System.out.println("Java ist plööht!");
     }
 }
