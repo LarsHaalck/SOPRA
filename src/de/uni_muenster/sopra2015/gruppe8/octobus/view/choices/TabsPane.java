d8fc1f8a3e9faeec848b5673cea3b5e39ff3291c
Jonas Hilbert <j_hilb04@wwu.de>
2015-03-02 15:41:13 +0100

    Login-Fenster als Dialog erstellt (Achtung: Enthält eine main-Methode)

--- src/de/uni_muenster/sopra2015/gruppe8/octobus/view/choices/TabsPane.java
+++ src/de/uni_muenster/sopra2015/gruppe8/octobus/view/choices/TabsPane.java
@@ -1,7 +1,6 @@
 package de.uni_muenster.sopra2015.gruppe8.octobus.view.choices;
 
 import de.uni_muenster.sopra2015.gruppe8.octobus.view.tabs.BusesTab;
-import de.uni_muenster.sopra2015.gruppe8.octobus.view.tabs.ExampleTab;
 
 import javax.swing.*;
 
@@ -13,7 +12,7 @@ public class TabsPane extends JTabbedPane
     public TabsPane(){
         super();
 
-        addTab("Allgemein", new ExampleTab());
+
         addTab("Allgemein", new JPanel());
         addTab("Allgemein", new JPanel());
         addTab("Allgemein", new JPanel());
