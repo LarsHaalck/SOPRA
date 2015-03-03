95328d4004a8077e954cc14cc3dc4161e854e380
Joshua <Joshua.Es@gmx.de>
2015-03-03 15:14:44 +0100

    Formatted Emmiters, added window emmiters

--- src/de/uni_muenster/sopra2015/gruppe8/octobus/view/listeners/EmitterUserState.java
+++ src/de/uni_muenster/sopra2015/gruppe8/octobus/view/listeners/EmitterUserState.java
@@ -1,7 +1,8 @@
 package de.uni_muenster.sopra2015.gruppe8.octobus.view.listeners;
 
 /**
- * Created by Lars on 03-Mar-15.
+ * EmitterUserState is an enum used to describe user states.
+ * Each value represents a unique user state.
  */
 public enum EmitterUserState
 {
