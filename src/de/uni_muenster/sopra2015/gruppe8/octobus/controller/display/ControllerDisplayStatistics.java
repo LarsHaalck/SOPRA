d493ad070fb66aa94390b1a67145204006d0d34d
Joshua <Joshua.Es@gmx.de>
2015-03-03 10:38:58 +0100

    Renamed display controllers

--- src/de/uni_muenster/sopra2015/gruppe8/octobus/controller/display/ControllerDisplayStatistics.java
+++ src/de/uni_muenster/sopra2015/gruppe8/octobus/controller/display/ControllerDisplayStatistics.java
@@ -3,6 +3,6 @@ package de.uni_muenster.sopra2015.gruppe8.octobus.controller.display;
 /**
  * Created by Lars on 02-Mar-15.
  */
-public class ControllerStatisticsDisplay
+public class ControllerDisplayStatistics
 {
 }
