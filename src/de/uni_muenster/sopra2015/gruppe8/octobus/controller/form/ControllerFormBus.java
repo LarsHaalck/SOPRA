b817c4d36c851d5cf22eac93bcdaaf7701794052
Joshua <Joshua.Es@gmx.de>
2015-03-03 15:01:09 +0100

    Added controller to FormBus

--- src/de/uni_muenster/sopra2015/gruppe8/octobus/controller/form/ControllerFormBus.java
+++ src/de/uni_muenster/sopra2015/gruppe8/octobus/controller/form/ControllerFormBus.java
@@ -1,8 +1,53 @@
 package de.uni_muenster.sopra2015.gruppe8.octobus.controller.form;
 
+import de.uni_muenster.sopra2015.gruppe8.octobus.controller.Controller;
+import de.uni_muenster.sopra2015.gruppe8.octobus.controller.ControllerManager;
+import de.uni_muenster.sopra2015.gruppe8.octobus.view.forms.FormBus;
+import de.uni_muenster.sopra2015.gruppe8.octobus.view.listeners.EmitterButton;
+import de.uni_muenster.sopra2015.gruppe8.octobus.view.listeners.ListenerButton;
+
 /**
  * Created by Lars on 02-Mar-15.
  */
-public class ControllerFormBus
+public class ControllerFormBus extends Controller implements ListenerButton
 {
+	private FormBus formBus;
+
+	public ControllerFormBus(FormBus formBus){
+		super();
+		this.formBus = formBus;
+	}
+
+	@Override
+	public void buttonPressed(EmitterButton emitter)
+	{
+		switch (emitter)
+		{
+			case FORM_BUS_SAVE:
+				//TODO finish this
+				closeDialog();
+				break;
+			case FORM_BUS_CANCEL:
+				//TODO finish this
+				closeDialog();
+				break;
+		}
+	}
+
+	@Override
+	protected void addListeners()
+	{
+		ControllerManager.addListener((ListenerButton) this);
+	}
+
+	@Override
+	protected void removeListeners()
+	{
+		ControllerManager.removeListener((ListenerButton) this);
+	}
+	private void closeDialog()
+	{
+		//TODO formBus.dispose();
+		removeListeners();
+	}
 }
--- src/de/uni_muenster/sopra2015/gruppe8/octobus/view/forms/FormBus.java
+++ src/de/uni_muenster/sopra2015/gruppe8/octobus/view/forms/FormBus.java
@@ -1,5 +1,8 @@
 package de.uni_muenster.sopra2015.gruppe8.octobus.view.forms;
 
+import de.uni_muenster.sopra2015.gruppe8.octobus.controller.form.ControllerFormBus;
+import de.uni_muenster.sopra2015.gruppe8.octobus.view.listeners.EmitterButton;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -9,6 +12,8 @@ import java.awt.*;
  */
 public class FormBus extends JPanel
 {
+	private ControllerFormBus controllerFormBus;
+
 	private String explanationText = "Bitte geben Sie die Daten des Busses ein.";
 	private JLabel explanation = new JLabel(explanationText);
 	private JPanel explanationPanel = new JPanel();
@@ -37,6 +42,15 @@ public class FormBus extends JPanel
 
 	public FormBus()
 	{
+		controllerFormBus = new ControllerFormBus(this);
+
+		save.addActionListener(e -> {
+			controllerFormBus.buttonPressed(EmitterButton.FORM_BUS_SAVE);
+		});
+		cancel.addActionListener(e -> {
+			controllerFormBus.buttonPressed(EmitterButton.FORM_BUS_CANCEL);
+		});
+
 		setLayout(new BorderLayout());
 		add(explanationPanel, BorderLayout.NORTH);
 		add(midPanel, BorderLayout.CENTER);
