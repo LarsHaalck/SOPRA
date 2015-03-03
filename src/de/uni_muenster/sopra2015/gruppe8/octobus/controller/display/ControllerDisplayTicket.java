5291377141dbdcad813007aa0726d7b26eb9c754
Florian Küpper <florian.kuepper@googlemail.com>
2015-03-03 15:12:15 +0100

    Added controller to DisplayTicket

--- src/de/uni_muenster/sopra2015/gruppe8/octobus/controller/display/ControllerDisplayTicket.java
+++ src/de/uni_muenster/sopra2015/gruppe8/octobus/controller/display/ControllerDisplayTicket.java
@@ -1,8 +1,29 @@
 package de.uni_muenster.sopra2015.gruppe8.octobus.controller.display;
 
+import de.uni_muenster.sopra2015.gruppe8.octobus.controller.Controller;
+import de.uni_muenster.sopra2015.gruppe8.octobus.view.displays.DisplayTicket;
+
 /**
  * Created by Lars on 02-Mar-15.
  */
-public class ControllerDisplayTicket
+public class ControllerDisplayTicket extends Controller
 {
+	DisplayTicket panel;
+
+	public ControllerDisplayTicket(DisplayTicket panel)
+	{
+		this.panel = panel;
+	}
+
+	@Override
+	protected void addListeners()
+	{
+
+	}
+
+	@Override
+	protected void removeListeners()
+	{
+
+	}
 }
