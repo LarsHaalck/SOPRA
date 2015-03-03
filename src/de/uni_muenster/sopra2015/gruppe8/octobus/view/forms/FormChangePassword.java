b176fd9be10794e096cb31a3f3f1bc312bde36f5
Lars Haalck <l_haal02@uni-muenster.de>
2015-03-03 13:14:35 +0100

    fixed Jonas' code style

--- src/de/uni_muenster/sopra2015/gruppe8/octobus/view/forms/FormChangePassword.java
+++ src/de/uni_muenster/sopra2015/gruppe8/octobus/view/forms/FormChangePassword.java
@@ -13,162 +13,162 @@ public class FormChangePassword extends JDialog
 {
 	private ControllerFormChangePassword controllerFormChangePassword;
 
-    private JPanel panel;
-    private GridBagConstraints cs;
-    private JLabel lbOldPassword, lbNewPassword, lbNewPasswordCorrect, lbOldPassword_Error,
-            lbNewPassword_Error, lbNewPasswordCorrect_Error;
-    private JTextField tfOldPassword, tfNewPassword, tfNewPasswordCorrect;
-    private JButton btnSave, btnCancel;
+	private JPanel panel;
+	private GridBagConstraints cs;
+	private JLabel lbOldPassword, lbNewPassword, lbNewPasswordCorrect, lbOldPassword_Error,
+			lbNewPassword_Error, lbNewPasswordCorrect_Error;
+	private JTextField tfOldPassword, tfNewPassword, tfNewPasswordCorrect;
+	private JButton btnSave, btnCancel;
 
-    public FormChangePassword(Frame parent)
-    {
-        super(parent, "Passwort ändern", true);
+	public FormChangePassword(Frame parent)
+	{
+		super(parent, "Passwort ändern", true);
 
 		controllerFormChangePassword = new ControllerFormChangePassword(this);
 
-        try
-        {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (UnsupportedLookAndFeelException e)
-        {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e)
-        {
-            e.printStackTrace();
-        } catch (InstantiationException e)
-        {
-            e.printStackTrace();
-        } catch (IllegalAccessException e)
-        {
-            e.printStackTrace();
-        }
+		try
+		{
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (UnsupportedLookAndFeelException e)
+		{
+			e.printStackTrace();
+		} catch (ClassNotFoundException e)
+		{
+			e.printStackTrace();
+		} catch (InstantiationException e)
+		{
+			e.printStackTrace();
+		} catch (IllegalAccessException e)
+		{
+			e.printStackTrace();
+		}
 
-        panel = new JPanel(new GridBagLayout());
-        cs = new GridBagConstraints();
+		panel = new JPanel(new GridBagLayout());
+		cs = new GridBagConstraints();
 
-        cs.fill = GridBagConstraints.HORIZONTAL;
+		cs.fill = GridBagConstraints.HORIZONTAL;
 
-        lbOldPassword = new JLabel("Altes Passwort: ");
-        cs.gridx = 0;
-        cs.gridy = 0;
-        cs.gridwidth = 1;
-        panel.add(lbOldPassword, cs);
+		lbOldPassword = new JLabel("Altes Passwort: ");
+		cs.gridx = 0;
+		cs.gridy = 0;
+		cs.gridwidth = 1;
+		panel.add(lbOldPassword, cs);
 
-        tfOldPassword = new JTextField(20);
-        cs.gridx = 1;
-        cs.gridy = 0;
-        cs.gridwidth = 2;
-        panel.add(tfOldPassword, cs);
+		tfOldPassword = new JTextField(20);
+		cs.gridx = 1;
+		cs.gridy = 0;
+		cs.gridwidth = 2;
+		panel.add(tfOldPassword, cs);
 
-        cs.fill = GridBagConstraints.CENTER;
+		cs.fill = GridBagConstraints.CENTER;
 
-        lbOldPassword_Error = new JLabel(" ");
-        lbOldPassword_Error.setForeground(Color.red);
-        cs.gridx = 0;
-        cs.gridy = 1;
-        cs.gridwidth = 3;
-        panel.add(lbOldPassword_Error, cs);
+		lbOldPassword_Error = new JLabel(" ");
+		lbOldPassword_Error.setForeground(Color.red);
+		cs.gridx = 0;
+		cs.gridy = 1;
+		cs.gridwidth = 3;
+		panel.add(lbOldPassword_Error, cs);
 
-        cs.fill = GridBagConstraints.HORIZONTAL;
+		cs.fill = GridBagConstraints.HORIZONTAL;
 
-        lbNewPassword = new JLabel("Neues Passwort: ");
-        cs.gridx = 0;
-        cs.gridy = 2;
-        cs.gridwidth = 1;
-        panel.add(lbNewPassword, cs);
+		lbNewPassword = new JLabel("Neues Passwort: ");
+		cs.gridx = 0;
+		cs.gridy = 2;
+		cs.gridwidth = 1;
+		panel.add(lbNewPassword, cs);
 
-        tfNewPassword = new JTextField(20);
-        tfNewPassword.setToolTipText("Das Passwort muss aus 8 Zeichen bestehen");
-        cs.gridx = 1;
-        cs.gridy = 2;
-        cs.gridwidth = 2;
-        panel.add(tfNewPassword, cs);
+		tfNewPassword = new JTextField(20);
+		tfNewPassword.setToolTipText("Das Passwort muss aus 8 Zeichen bestehen");
+		cs.gridx = 1;
+		cs.gridy = 2;
+		cs.gridwidth = 2;
+		panel.add(tfNewPassword, cs);
 
-        cs.fill = GridBagConstraints.CENTER;
+		cs.fill = GridBagConstraints.CENTER;
 
-        lbNewPassword_Error = new JLabel(" ");
-        lbNewPassword_Error.setForeground(Color.red);
-        cs.gridx = 0;
-        cs.gridy = 3;
-        cs.gridwidth = 3;
-        panel.add(lbNewPassword_Error, cs);
+		lbNewPassword_Error = new JLabel(" ");
+		lbNewPassword_Error.setForeground(Color.red);
+		cs.gridx = 0;
+		cs.gridy = 3;
+		cs.gridwidth = 3;
+		panel.add(lbNewPassword_Error, cs);
 
-        cs.fill = GridBagConstraints.HORIZONTAL;
+		cs.fill = GridBagConstraints.HORIZONTAL;
 
-        lbNewPasswordCorrect = new JLabel("Erneut eingeben: ");
-        cs.gridx = 0;
-        cs.gridy = 4;
-        cs.gridwidth = 1;
-        panel.add(lbNewPasswordCorrect, cs);
+		lbNewPasswordCorrect = new JLabel("Erneut eingeben: ");
+		cs.gridx = 0;
+		cs.gridy = 4;
+		cs.gridwidth = 1;
+		panel.add(lbNewPasswordCorrect, cs);
 
-        tfNewPasswordCorrect = new JTextField(20);
-        cs.gridx = 1;
-        cs.gridy = 4;
-        cs.gridwidth = 2;
-        panel.add(tfNewPasswordCorrect, cs);
+		tfNewPasswordCorrect = new JTextField(20);
+		cs.gridx = 1;
+		cs.gridy = 4;
+		cs.gridwidth = 2;
+		panel.add(tfNewPasswordCorrect, cs);
 
-        cs.fill = GridBagConstraints.CENTER;
+		cs.fill = GridBagConstraints.CENTER;
 
 
-        lbNewPasswordCorrect_Error = new JLabel(" ");
-        lbNewPasswordCorrect_Error.setForeground(Color.red);
-        cs.gridx = 0;
-        cs.gridy = 5;
-        cs.gridwidth = 3;
-        panel.add(lbNewPasswordCorrect_Error, cs);
+		lbNewPasswordCorrect_Error = new JLabel(" ");
+		lbNewPasswordCorrect_Error.setForeground(Color.red);
+		cs.gridx = 0;
+		cs.gridy = 5;
+		cs.gridwidth = 3;
+		panel.add(lbNewPasswordCorrect_Error, cs);
 
-        btnSave = new JButton("Speichern");
-        btnSave.addActionListener(e -> {
-            controllerFormChangePassword.buttonPressed(EmitterButton.FORM_CHANGE_PASSWORD_SAVE);
-        });
+		btnSave = new JButton("Speichern");
+		btnSave.addActionListener(e -> {
+			controllerFormChangePassword.buttonPressed(EmitterButton.FORM_CHANGE_PASSWORD_SAVE);
+		});
 
-        btnCancel = new JButton("Cancel");
-        btnCancel.addActionListener(e -> {
+		btnCancel = new JButton("Cancel");
+		btnCancel.addActionListener(e -> {
 			controllerFormChangePassword.buttonPressed(EmitterButton.FORM_CHANGE_PASSWORD_CANCEL);
-        });
-        JPanel bp = new JPanel();
-        bp.add(btnSave);
-        bp.add(btnCancel);
+		});
+		JPanel bp = new JPanel();
+		bp.add(btnSave);
+		bp.add(btnCancel);
 
-        getContentPane().add(panel, BorderLayout.CENTER);
-        getContentPane().add(bp, BorderLayout.PAGE_END);
+		getContentPane().add(panel, BorderLayout.CENTER);
+		getContentPane().add(bp, BorderLayout.PAGE_END);
 
-        pack();
-        setResizable(false);
-        setLocationRelativeTo(parent);
-        getRootPane().setDefaultButton(btnSave);
+		pack();
+		setResizable(false);
+		setLocationRelativeTo(parent);
+		getRootPane().setDefaultButton(btnSave);
 
-        illegalChanges(false, false, true);
-    }
+		illegalChanges(false, false, true);
+	}
 
-    public void illegalChanges(boolean OldPassword, boolean NewPassword, boolean NewPasswordCorrect)
-    {
-        if (OldPassword)
-            lbOldPassword_Error.setText("Das Passwort ist falsch!");
-        else
-            lbOldPassword_Error.setText(" ");
-        if (NewPassword)
-            lbNewPassword_Error.setText("Das Passwort ist falsch!");
-        else
-            lbNewPassword_Error.setText(" ");
-        if (NewPasswordCorrect)
-            lbNewPasswordCorrect_Error.setText("Das Passwort ist falsch!");
-        else
-            lbNewPasswordCorrect_Error.setText(" ");
-    }
+	public void illegalChanges(boolean OldPassword, boolean NewPassword, boolean NewPasswordCorrect)
+	{
+		if (OldPassword)
+			lbOldPassword_Error.setText("Das Passwort ist falsch!");
+		else
+			lbOldPassword_Error.setText(" ");
+		if (NewPassword)
+			lbNewPassword_Error.setText("Das Passwort ist falsch!");
+		else
+			lbNewPassword_Error.setText(" ");
+		if (NewPasswordCorrect)
+			lbNewPasswordCorrect_Error.setText("Das Passwort ist falsch!");
+		else
+			lbNewPasswordCorrect_Error.setText(" ");
+	}
 
-    public String getOldPassword()
-    {
-        return tfOldPassword.getText().trim();
-    }
+	public String getOldPassword()
+	{
+		return tfOldPassword.getText().trim();
+	}
 
-    public String getNewPassword()
-    {
-        return tfNewPassword.getText().trim();
-    }
+	public String getNewPassword()
+	{
+		return tfNewPassword.getText().trim();
+	}
 
-    public String getNewPasswordCorrect()
-    {
-        return tfNewPasswordCorrect.getText().trim();
-    }
+	public String getNewPasswordCorrect()
+	{
+		return tfNewPasswordCorrect.getText().trim();
+	}
 }
