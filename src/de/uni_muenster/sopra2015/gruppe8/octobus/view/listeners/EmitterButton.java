fff3d83612e0f1ef05afb87a634bec491f96f646
Joshua <Joshua.Es@gmx.de>
2015-03-03 15:10:53 +0100

    Formatted EmmiterButton

--- src/de/uni_muenster/sopra2015/gruppe8/octobus/view/listeners/EmitterButton.java
+++ src/de/uni_muenster/sopra2015/gruppe8/octobus/view/listeners/EmitterButton.java
@@ -1,7 +1,8 @@
 package de.uni_muenster.sopra2015.gruppe8.octobus.view.listeners;
 
 /**
- * Created by Lars on 03-Mar-15.
+ * EmitterButton is an enum used to describe buttons.
+ * Each button has an unique value consisting of the source class and his name
  */
 public enum EmitterButton
 {
@@ -9,16 +10,22 @@ public enum EmitterButton
 	PANEL_PASSENGER_SEARCH_CONNECTION,
 	PANEL_PASSENGER_SHOW_TICKETS,
 	PANEL_PASSENGER_LOGIN,
+
 	PANEL_EMPLOYEE_LOGOUT,
 	PANEL_EMPLOYEE_CHANGE_PASSWORD,
+
 	FORM_LOGIN_LOGIN,
 	FORM_LOGIN_CANCEL,
+
 	FORM_CHANGE_PASSWORD_SAVE,
 	FORM_CHANGE_PASSWORD_CANCEL,
+
 	FORM_BUS_SAVE,
 	FORM_BUS_CANCEL,
+
 	FORM_BUS_STOP_SAVE,
 	FORM_BUS_STOP_CANCEL,
+
 	FORM_EMPLOYEE_SAVE,
 	FORM_EMPLOYEE_CANCEL
 }
