2a243ff5cf6ce6272d2ee010a601ae6967820c7a
Florian Küpper <florian.kuepper@googlemail.com>
2015-03-05 10:54:47 +0100

    Added error-handling to controller-form-bus.

--- src/de/uni_muenster/sopra2015/gruppe8/octobus/controller/Controller.java
+++ src/de/uni_muenster/sopra2015/gruppe8/octobus/controller/Controller.java
@@ -1,5 +1,7 @@
 package de.uni_muenster.sopra2015.gruppe8.octobus.controller;
 
+import java.util.ArrayList;
+
 /**
  * Abstract Controller to ensure that each controller will add itself to the ControllerManager with its constructor
  * and that each controller has the ability to remove itself from the ControllerManager.
@@ -20,5 +22,34 @@ public abstract class Controller
 	 * Removes every Listener implemented by the controller from ControllerManager
 	 */
 	protected abstract void removeListeners();
+
+	/**
+	 * Helper-method. Parses a string to an integer
+	 * @param s String to parese.
+	 * @return String as integer, or -1 if it fails
+	 */
+	protected int parseInt(String s)
+	{
+		try
+		{
+			int i = Integer.parseInt(s);
+			return i;
+		}
+		catch (Exception e)
+		{
+			return -1;
+		}
+	}
+
+	protected String errorListToString(ArrayList<String> errors)
+	{
+		StringBuilder builder = new StringBuilder();
+		builder.append("      " + errors.remove(0));
+		for( String s : errors) {
+			builder.append( "\n");
+			builder.append("      "+s);
+		}
+		return builder.toString();
+	}
 
 }
--- src/de/uni_muenster/sopra2015/gruppe8/octobus/controller/form/ControllerFormBus.java
+++ src/de/uni_muenster/sopra2015/gruppe8/octobus/controller/form/ControllerFormBus.java
@@ -7,7 +7,9 @@ import de.uni_muenster.sopra2015.gruppe8.octobus.view.forms.FormBus;
 import de.uni_muenster.sopra2015.gruppe8.octobus.controller.listeners.EmitterButton;
 import de.uni_muenster.sopra2015.gruppe8.octobus.controller.listeners.ListenerButton;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.LinkedList;
 
 /**
  * Created by Lars on 02-Mar-15.
@@ -34,8 +36,13 @@ public class ControllerFormBus extends Controller implements ListenerButton
 		switch (emitter)
 		{
 			case FORM_BUS_SAVE:
-				//TODO finish this
-				closeDialog();
+				if(parseValuesFromForm())
+				{
+					if(saveToDB())
+					{
+						closeDialog();
+					}
+				}
 				break;
 			case FORM_BUS_CANCEL:
 				closeDialog();
@@ -69,6 +76,66 @@ public class ControllerFormBus extends Controller implements ListenerButton
 			formBus.setNextInspectionDue("" + bus.getNextInspectionDue());
 			formBus.setArticulatedBus(bus.isArticulatedBus());
 		}
+	}
+
+	private boolean parseValuesFromForm()
+	{
+		if(objectID == -1)
+		{
+			//TODO empty constructor
+			bus = new Bus("TestNummernschild", 42, 32, "Ich", "cooler Bus", new Date(2015, 5, 29), true);
+		}
+		String licencePlate = formBus.getLicencePlate();
+		int numberOfSeats = parseInt(formBus.getNumberOfSeats());
+		int standingRoom = parseInt(formBus.getStandingRoom());
+		String manufacturer = formBus.getManufacturer();
+		String model = formBus.getModel();
+		Date nextInspectionDue = null;
+		boolean articulatedBus = formBus.getArticulatedBus();
+
+		ArrayList<String> errorFields = new ArrayList<>();
+		if(licencePlate.trim().length() == 0)
+			errorFields.add("Das Kennzeichen darf nicht leer sein.");
+		else if(licencePlate.trim().length() < 5)
+			errorFields.add("Das Kennzeichen muss mindestens 5 Zeichen umfassen.");
+		else if(licencePlate.length() > 20)
+			errorFields.add("Das Kennzeichen darf nicht mehr als 20 Zeichen umfassen.");
+		if(numberOfSeats == -1)
+			errorFields.add("Die Anzahl der Sitzplätze ist keine gültige Zahl");
+		if(standingRoom == -1)
+			errorFields.add("Die Anzahl der Stehplätze ist keine gültige Zahl.");
+		if(manufacturer.trim().length() == 0)
+			errorFields.add("Der Hersteller-Name darf nicht leer sein.");
+		if(model.trim().length() == 0)
+			errorFields.add("Der Modell-Name darf nicht leer sein.");
+		if(nextInspectionDue == null)
+			errorFields.add("Das Datum der nächsten Inspektion ist in keinem gültigen Format.");
+
+		if(errorFields.size() > 0)
+		{
+			String errorMessage = "Die eingegeben Daten sind nicht gültig.\n";
+			errorMessage += errorListToString(errorFields);
+			formBus.showErrorForm(errorMessage);
+			return false;
+		}
+		else
+		{
+			bus.setLicencePlate(licencePlate);
+			bus.setNumberOfSeats(numberOfSeats);
+			bus.setStandingRoom(standingRoom);
+			bus.setManufacturer(manufacturer);
+			bus.setModel(model);
+			bus.setNextInspectionDue(nextInspectionDue);
+			bus.setArticulatedBus(articulatedBus);
+			//TODO Update into databse
+			return true;
+		}
+		//Datenbank.updateBus(Bus-Objekt, id)
+	}
+
+	private boolean saveToDB()
+	{
+		return true;
 	}
 
 	@Override
