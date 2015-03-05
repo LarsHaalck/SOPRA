25f4b4b0092c1b8001d59944c347f0b6ac39cf6e
Patricia <PatriciaSchinke92@gmail.com>
2015-03-05 16:14:16 +0100

    Changed Layout to GridBagLayout

--- src/de/uni_muenster/sopra2015/gruppe8/octobus/view/forms/FormTicket.java
+++ src/de/uni_muenster/sopra2015/gruppe8/octobus/view/forms/FormTicket.java
@@ -2,6 +2,9 @@ package de.uni_muenster.sopra2015.gruppe8.octobus.view.forms;
 
 import de.uni_muenster.sopra2015.gruppe8.octobus.controller.form.ControllerFormTicket;
 import de.uni_muenster.sopra2015.gruppe8.octobus.controller.listeners.EmitterButton;
+import de.uni_muenster.sopra2015.gruppe8.octobus.view.text_fields.FieldDate;
+import de.uni_muenster.sopra2015.gruppe8.octobus.view.text_fields.FieldNumber;
+import de.uni_muenster.sopra2015.gruppe8.octobus.view.text_fields.FieldText;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
@@ -20,28 +23,15 @@ public class FormTicket extends FormGeneral
 	 */
 	private String strExplanation = "Bitte geben Sie die Daten des Tickets ein.";
 	private JLabel lbExplanation = new JLabel(strExplanation);
-	private JPanel plExplanation = new JPanel();
-
-	private JPanel plMid = new JPanel();
-	private JPanel plBottom = new JPanel();
 
 	/*
 	 * every input has an own label, inputfield and panel
 	 */
-	private JLabel lbName = new JLabel("Name");
-	private JLabel lbPrice = new JLabel("Preis");
-	private JLabel lbNumPassengers = new JLabel("Anzahl Fahrgäste");
-	private JLabel lbDescription = new JLabel("Beschreibung");
 
 	private JTextField tfName = new JTextField();
 	private JTextField tfPrice = new JTextField();
 	private JTextField tfNumPassengers = new JTextField();
 	private JTextArea taDescription = new JTextArea();
-
-	private JPanel plName = new JPanel();
-	private JPanel plPrice = new JPanel();
-	private JPanel plNumPassengers = new JPanel();
-	private JPanel plDescription = new JPanel();
 
 	/*
 	 * the buttons for save and cancel
@@ -49,17 +39,18 @@ public class FormTicket extends FormGeneral
 	private JButton btSave = new JButton("Speichern");
 	private JButton btCancel = new JButton("Abbrechen");
 
-	private int iTextHeight = 25;
-	private int iAreaHeight = 50;
-	private int iTextWidth = 150;
 
 	public FormTicket(Frame parent, int objectID)
 	{
 		super(parent, "");
+
+		getRootPane().setBorder(new EmptyBorder(10,10,10,10));
+
 		if(objectID == -1)
 			setTitle("Ticket anlegen");
 		else
 			setTitle("Ticket ändern");
+		setResizable(false);
 
 		controllerFormTicket = new ControllerFormTicket(this);
 
@@ -72,52 +63,79 @@ public class FormTicket extends FormGeneral
 			controllerFormTicket.buttonPressed(EmitterButton.FORM_TICKET_CANCEL);
 		});
 
-		setLayout(new BorderLayout());
-		add(plExplanation, BorderLayout.NORTH);
-		add(plMid, BorderLayout.CENTER);
-		add(plBottom, BorderLayout.SOUTH);
 
-		plExplanation.setPreferredSize(new Dimension(924, 100));
-		plExplanation.setBorder(new EmptyBorder(new Insets(40, 0, 40, 0)));
-		plExplanation.add(lbExplanation);
 
-		plMid.setLayout(new BoxLayout(plMid, BoxLayout.Y_AXIS));
-		plMid.add(plName);
-		plName.setLayout(new FlowLayout());
-		plMid.add(plPrice);
-		plPrice.setLayout(new FlowLayout());
-		plMid.add(plNumPassengers);
-		plNumPassengers.setLayout(new FlowLayout());
-		plMid.add(plDescription);
-		plDescription.setLayout(new FlowLayout());
 
-		plName.add(lbName);
-		plName.add(tfName);
-		plPrice.add(lbPrice);
-		plPrice.add(tfPrice);
-		plNumPassengers.add(lbNumPassengers);
-		plNumPassengers.add(tfNumPassengers);
-		plDescription.add(lbDescription);
+		setLayout(new GridBagLayout());
+		GridBagConstraints cstLabel = new GridBagConstraints();
+		GridBagConstraints cstTextField = new GridBagConstraints();
 
+		//general constraints
+		cstTextField.fill = GridBagConstraints.HORIZONTAL;
+		cstLabel.ipadx = 20;
+		cstLabel.ipady = 20;
+		cstLabel.anchor = GridBagConstraints.LINE_START;
+
+		cstLabel.gridwidth = 4;
+		cstLabel.gridx = 0;
+		cstLabel.gridy = 0;
+		String strExplanation = "Bitte geben Sie die Daten des Tickets ein.";
+		JLabel lbExplanation = new JLabel(strExplanation);
+		add(lbExplanation, cstLabel);
+
+		cstLabel.gridwidth = 2;
+		cstLabel.gridx = 0;
+		cstLabel.gridy = 1;
+		JLabel lbName = new JLabel("Name");
+		add(lbName, cstLabel);
+
+		cstTextField.gridwidth = 2;
+		cstTextField.gridx = 2;
+		cstTextField.gridy = 1;
+		tfName = new FieldText(10);
+		add(tfName, cstTextField);
+
+		cstLabel.gridy = 2;
+		JLabel lbPrice = new JLabel("Preis");
+		add(lbPrice, cstLabel);
+
+		cstTextField.gridy = 2;
+		tfPrice = new FieldNumber(3);
+		add(tfPrice, cstTextField);
+
+		cstLabel.gridy = 3;
+		JLabel lbNumPassengers = new JLabel("Anzahl Fahrgäste");
+		add(lbNumPassengers, cstLabel);
+
+		cstTextField.gridy = 3;
+		tfNumPassengers = new FieldNumber(3);
+		add(tfNumPassengers, cstTextField);
+
+		cstLabel.ipady=80;
+		cstLabel.gridy = 4;
+		JLabel lbDescription = new JLabel("Beschreibung");
+		add(lbDescription, cstLabel);
+
+		cstTextField.ipady=50;
+		cstTextField.gridy = 4;
 		JScrollPane scrollPane = new JScrollPane(taDescription);
-		plDescription.add(scrollPane);
-		scrollPane.setPreferredSize(new Dimension(iTextWidth, iAreaHeight));
+		add(scrollPane, cstTextField);
 
-		lbName.setPreferredSize(new Dimension(iTextWidth, iTextHeight));
-		tfName.setPreferredSize(new Dimension(iTextWidth, iTextHeight));
-		lbPrice.setPreferredSize(new Dimension(iTextWidth, iTextHeight));
-		tfPrice.setPreferredSize(new Dimension(iTextWidth, iTextHeight));
-		lbNumPassengers.setPreferredSize(new Dimension(iTextWidth, iTextHeight));
-		tfNumPassengers.setPreferredSize(new Dimension(iTextWidth, iTextHeight));
-		lbDescription.setPreferredSize(new Dimension(iTextWidth, iTextHeight));
+		GridBagConstraints cstButton = new GridBagConstraints();
+		cstButton.gridwidth = 1;
 
-		plBottom.setLayout(new BorderLayout());
-		plBottom.setBorder(new EmptyBorder(new Insets(30, 60, 30, 60)));
-		plBottom.setPreferredSize(new Dimension(924, 100));
-		plBottom.add(btCancel, BorderLayout.WEST);
-		plBottom.add(btSave, BorderLayout.EAST);
+		cstButton.gridx = 0;
+		cstButton.gridy = 5;
+		cstButton.anchor = GridBagConstraints.LAST_LINE_START;
+		add(btCancel, cstButton);
+
+		cstButton.gridx = 3;
+		cstButton.gridy = 5;
+		cstButton.anchor = GridBagConstraints.LAST_LINE_END;
+		add(btSave, cstButton);
 
 		pack();
+		setLocationRelativeTo(null);
 	}
 
 	public String getNameTicket()
