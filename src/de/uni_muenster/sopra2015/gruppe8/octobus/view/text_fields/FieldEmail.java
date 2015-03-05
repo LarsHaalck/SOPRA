7da4959d957ade9555302f42f8010b3f2068dbd6
Lars Haalck <l_haal02@uni-muenster.de>
2015-03-05 14:49:03 +0100

    FieldEmail ignore ' and = (basic SQL-injections)

--- src/de/uni_muenster/sopra2015/gruppe8/octobus/view/text_fields/FieldEmail.java
+++ src/de/uni_muenster/sopra2015/gruppe8/octobus/view/text_fields/FieldEmail.java
@@ -1,5 +1,7 @@
 package de.uni_muenster.sopra2015.gruppe8.octobus.view.text_fields;
 
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -8,6 +10,17 @@ public class FieldEmail extends FieldText
 	public FieldEmail()
 	{
 		super();
+		this.addKeyListener(new KeyAdapter()
+		{
+			public void keyTyped(KeyEvent e) //ignore some cases to prevent SQL-injections
+			{
+				char c = e.getKeyChar();
+				if (c == '\'' || c == '=')
+				{
+					e.consume();  // ignore event
+				}
+			}
+		});
 	}
 
 	private boolean isValidEmail()
