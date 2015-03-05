9bd4275a4b3927844d4db637200760b0d99dcb94
Lars Haalck <l_haal02@CLETUS.uni-muenster.de>
2015-03-05 20:49:32 +0100

    prepared FieldDate for red Border for incorrect inputs

--- src/de/uni_muenster/sopra2015/gruppe8/octobus/view/text_fields/FieldDate.java
+++ src/de/uni_muenster/sopra2015/gruppe8/octobus/view/text_fields/FieldDate.java
@@ -15,6 +15,8 @@ import java.util.regex.Pattern;
 
 public class FieldDate extends FieldText
 {
+    private int year, month, day;
+
 	public FieldDate()
 	{
 		super(11);
@@ -36,16 +38,8 @@ public class FieldDate extends FieldText
 	private boolean isValidDate()
 	{
 		if (Pattern.compile("([0-9]{1,2}).([0-9]{1,2}).([0-9]{4})").matcher(this.getText()).matches()) //regex for date
-			return true;
-		else
-			return false;
-	}
+        {
 
-
-	public Date getDate()
-	{
-		if(isValidDate())
-		{
 			String input = this.getText();
 			int indexDot = input.indexOf('.');
 			int day = Integer.parseInt(input.substring(0, indexDot));
@@ -59,7 +53,7 @@ public class FieldDate extends FieldText
 			int year = Integer.parseInt(input.substring(0));
 
 			if(year < 0 || month < 1 || month > 12 || day > 31 || day < 1)
-				return null;
+				return false;
 
 			boolean isLeapYear = ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0));
 			switch (month)
@@ -77,12 +71,25 @@ public class FieldDate extends FieldText
 				case 6:
 				case 9:
 				case 11:
-					if(day > 30) return null;
+					if(day > 30) return false;
 					break;
 				case 2:
-					if(isLeapYear && day > 29) return null;
-					else if(!isLeapYear && day > 28) return null;
+					if(isLeapYear && day > 29) return false;
+					else if(!isLeapYear && day > 28) return false;
 			}
+			return true;
+        }
+		else
+        {
+			return false;
+        }
+	}
+
+
+	public Date getDate()
+	{
+		if(isValidDate())
+		{
 
 			Calendar c = Calendar.getInstance();
 			c.set(year, month - 1, day, 0, 0);
@@ -98,4 +105,14 @@ public class FieldDate extends FieldText
 		SimpleDateFormat df = new SimpleDateFormat("dd.MM.yyyy");
 		this.setText(df.format(date));
 	}
+
+    public void setRedBorder(boolean toggle)
+    {
+        if(toogle)
+        {
+            this.setBorder(BorderFactory.createLineBorder(Color.red));
+        }
+        else
+            this.setBorder(null);
+    }
 }
