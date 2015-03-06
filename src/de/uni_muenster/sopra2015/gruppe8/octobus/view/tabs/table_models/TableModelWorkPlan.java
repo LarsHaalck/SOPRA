af26c5b921cb920090e4e3537fe7cedfe41704bd
Florian Küpper <florian.kuepper@googlemail.com>
2015-03-06 11:29:01 +0100

    Show work-plan in tab.

---
+++ src/de/uni_muenster/sopra2015/gruppe8/octobus/view/tabs/table_models/TableModelWorkPlan.java
@@ -0,0 +1,24 @@
+package de.uni_muenster.sopra2015.gruppe8.octobus.view.tabs.table_models;
+
+/**
+ * Created by Florian on 06.03.2015.
+ */
+public class TableModelWorkPlan extends ExtendedTableModel
+{
+	public TableModelWorkPlan()
+	{
+		//Add column-names from enum
+		ColumnsWorkPlan[] values = ColumnsWorkPlan.values();
+		columnNames = new String[values.length];
+		for (int i = 0; i < columnNames.length; i++)
+			columnNames[i] = values[i].toString();
+
+		data = new Object[][]{};
+	}
+
+	@Override
+	public String[] getRefineableColumns()
+	{
+		return new String[]{ColumnsWorkPlan.DATE.toString(), ColumnsWorkPlan.START.toString(), ColumnsWorkPlan.END.toString()};
+	}
+}
