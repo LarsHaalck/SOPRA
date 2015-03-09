0dec226fc9c3f798bdcb9e0aa24fe30c7379a361
Patricia <PatriciaSchinke92@gmail.com>
2015-03-09 15:44:59 +0100

    First ideas for the printing of stoppingPoint-departures

--- src/de/uni_muenster/sopra2015/gruppe8/octobus/view/print_views/PrintViewStoppingPoint.java
+++ src/de/uni_muenster/sopra2015/gruppe8/octobus/view/print_views/PrintViewStoppingPoint.java
@@ -1,9 +1,14 @@
 package de.uni_muenster.sopra2015.gruppe8.octobus.view.print_views;
 
+import de.uni_muenster.sopra2015.gruppe8.octobus.model.Route;
+import de.uni_muenster.sopra2015.gruppe8.octobus.model.StoppingPoint;
+import de.uni_muenster.sopra2015.gruppe8.octobus.model.print.PrintStoppingPoint;
+
 import java.awt.*;
 import java.awt.print.PageFormat;
 import java.awt.print.Printable;
 import java.awt.print.PrinterException;
+import java.time.DayOfWeek;
 
 /**
  * Created by Florian on 07.03.2015.
@@ -11,10 +16,80 @@ import java.awt.print.PrinterException;
 public class PrintViewStoppingPoint implements Printable
 {
 	private Graphics2D graphics2D;
+	private double pageHeight;
+	private double pageWidth;
+	private int numPages;
+	private Font fontHeader;
+	private Font fontHeader2;
+	private Font fontNormal;
+	private PrintStoppingPoint data;
+	private int entriesPerPage;
+	private final int entryHeight = 70;
+	private final int entryXStart = 60;
+	private final int entryYStart = 180;
 
-	@Override
+	private StoppingPoint stop;
+	private Route route;
+
+	public PrintViewStoppingPoint(PrintStoppingPoint data)
+	{
+		this.data = data;
+
+		fontHeader = new Font("Serif", Font.BOLD, 50);
+		fontHeader2 = new Font("Serif", Font.BOLD, 20);
+		fontNormal = new Font("Serif", Font.BOLD, 12);
+
+		stop = data.getStoppingPoint();
+		route = data.getRoute();
+	}
+
 	public int print(Graphics graphics, PageFormat pageFormat, int pageIndex) throws PrinterException
 	{
-		return 0;
+		graphics2D = (Graphics2D) graphics;
+
+		pageHeight = pageFormat.getImageableHeight();
+		pageWidth = pageFormat.getImageableWidth();
+		if (pageIndex >= numPages)
+		{
+			return NO_SUCH_PAGE;
+		}
+		drawHeader();
+		drawContent(pageIndex);
+		return PAGE_EXISTS;
+	}
+
+	private void drawHeader()
+	{
+		graphics2D.setFont(fontHeader);
+		graphics2D.drawString("OctoBUS", 140, 110);
+		graphics2D.setFont(fontHeader2);
+		graphics2D.drawString("Abfahrtszeiten von "+data.getStoppingPoint().getName()+" für Linie "+data.getRoute().getName(), 140,135);//zB Zeiten von Hbf für Linie 11
+	}
+
+	private void drawContent(int pageIndex)
+	{
+		long curX = entryXStart;
+		long curY = entryYStart;
+
+		graphics2D.setFont(fontNormal);
+
+		for (int i = pageIndex*entriesPerPage; i<entriesPerPage*(pageIndex + 1); i++)
+		{
+			for (DayOfWeek day:DayOfWeek.values())
+			{
+				data = new PrintStoppingPoint(stop, route, day);
+				String curDate = day + ":";
+				graphics2D.drawString(curDate, curX, curY);
+				curY += 20;
+				for (int time : data.getDepartureTimes())
+				{
+					curDate = time / 60 + ":" + time % 60;
+					graphics2D.drawString(curDate, curX, curY);
+					curY += 20;
+				}
+				curX += 100;
+				curY = entryYStart;
+			}
+		}
 	}
 }
