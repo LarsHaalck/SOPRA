2bf1ab95561cc3fe94fa594eaaa392ed526821b7
Patricia <PatriciaSchinke92@gmail.com>
2015-03-09 01:07:08 +0100

    DisplayTicket does look a bit better now and Linebeaks for the description

--- src/de/uni_muenster/sopra2015/gruppe8/octobus/view/displays/DisplayTicket.java
+++ src/de/uni_muenster/sopra2015/gruppe8/octobus/view/displays/DisplayTicket.java
@@ -4,19 +4,20 @@ import de.uni_muenster.sopra2015.gruppe8.octobus.controller.display.ControllerDi
 import de.uni_muenster.sopra2015.gruppe8.octobus.controller.listeners.EmitterButton;
 
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 /**
  * @author Patricia Schinke
- *         still does not get the Data
+ *
  */
 public class DisplayTicket extends JPanel
 {
 	//panel which shall show all tickets should be with scollpane
 	private JPanel plTable;
+	private JPanel plTickets;
 	private JButton btnBack;
 	private ControllerDisplayTicket controller;
+	private  GridBagConstraints cst;
 
 	public DisplayTicket()
 	{
@@ -24,6 +25,7 @@ public class DisplayTicket extends JPanel
 
 		setLayout(new BorderLayout(5,5));
 		plTable = new JPanel();
+		plTickets = new JPanel();
 		JScrollPane scrollPane = new JScrollPane(plTable);
 		add(scrollPane, BorderLayout.CENTER);
 
@@ -39,37 +41,66 @@ public class DisplayTicket extends JPanel
 		plButton.setLayout(new BorderLayout());
 		plButton.add(btnBack, BorderLayout.EAST);
 		add(plButton, BorderLayout.PAGE_END);
+		plTickets.setLayout(new GridBagLayout());
+		plTickets.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
+		cst = new GridBagConstraints();
 		controller.fill();
+		plTable.add(plTickets);
 	}
 
 
 	//adds Panel to the plTable with data from the dataBase (is used in ControllerFormShowTckets
-	public void addPanel(String name, int price, int numPassengers, String description){
-		JPanel plNew = new JPanel();
+	public void addPanel(String name, int price, int numPassengers, String description, int i){
 	//noch umranden und vllt mit Farben und anderen schriftgrößen schöner machen
-	//	plNew.setBackground(Color.lightGray);
-		plNew.setLayout(new GridBagLayout());
-		plNew.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
-		GridBagConstraints cst = new GridBagConstraints();
+		cst.gridy = i;
+		if(i>102)
+		{
+			JPanel plDistance = new JPanel();
+			plDistance.setBackground(Color.LIGHT_GRAY);
+			plDistance.setPreferredSize(new Dimension(982, 3));
+			plTickets.add(plDistance, cst);
+		}
 		cst.ipadx = 20;
 		cst.ipady = 20;
 		cst.gridx = 0;
-		cst.gridy = 0;
+		cst.gridy = i+1;
+		cst.gridwidth = 2;
 		JLabel lbName = new JLabel(name);
-		plNew.add(lbName, cst);
-		cst.gridx = 1;
-		JLabel lbPrice = new JLabel(""+price);
-		plNew.add(lbPrice, cst);
-		cst.gridx = 2;
-		JLabel lbNumPassengers = new JLabel(""+numPassengers);
-		plNew.add(lbNumPassengers, cst);
+		lbName.setFont(new Font("Arial", Font.BOLD, 16));
+		plTickets.add(lbName, cst);
 		cst.gridx = 0;
-		cst.gridy = 1;
-		cst.gridwidth = 3;
+		cst.gridy = i+2;
+		cst.gridwidth = 1;
+		cst.weightx = 1;
+		cst.anchor = GridBagConstraints.LINE_END; //does not work
+		JLabel lbPrice = new JLabel("Preis: "+price+"€");
+		lbPrice.setFont(new Font("Arial",Font.PLAIN,14));
+		plTickets.add(lbPrice, cst);
+		cst.gridx = 1;
+		cst.anchor = GridBagConstraints.LINE_START;
+		String strNumPas;
+		if(numPassengers == 1)
+		{
+			strNumPas = numPassengers+" Person";
+		}
+		else
+		{
+			strNumPas = numPassengers+" Personen";
+		}
+		JLabel lbNumPassengers = new JLabel(strNumPas);
+		lbNumPassengers.setFont(new Font("Arial",Font.PLAIN,14));
+		plTickets.add(lbNumPassengers, cst);
+		cst.gridx = 0;
+		cst.gridy = i+6;
+		cst.gridwidth = 2;
+		cst.anchor = GridBagConstraints.CENTER;
 		String[] lines = description.split("\n");
-		//TODO add every single line with for-each
-		JLabel lbDescription = new JLabel(description);
-		plNew.add(lbDescription, cst);
-		plTable.add(plNew);
+		for(String line: lines)
+		{
+			JLabel lbDescription = new JLabel(line);
+			plTickets.add(lbDescription, cst);
+			i++;
+			cst.gridy = i+2;
+		}
 	}
 }
