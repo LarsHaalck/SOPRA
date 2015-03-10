74d88ffabf8af8157dd6f8e62caf0e556ad6901f
Michael Biech <tikurion@gmail.com>
2015-03-10 21:54:27 +0100

    Blub. Wir sind doof :B.

--- src/de/uni_muenster/sopra2015/gruppe8/octobus/view/tabs/table_models/TableModelWorkPlan.java
+++ src/de/uni_muenster/sopra2015/gruppe8/octobus/view/tabs/table_models/TableModelWorkPlan.java
@@ -7,7 +7,7 @@ public class TableModelWorkPlan extends ExtendedTableModel
 {
 	public TableModelWorkPlan()
 	{
-		//Add column-names from enum
+        //Add column-names from enum
 		ColumnsWorkPlan[] values = ColumnsWorkPlan.values();
 		columnNames = new String[values.length];
 		for (int i = 0; i < columnNames.length; i++)
@@ -26,28 +26,5 @@ public class TableModelWorkPlan extends ExtendedTableModel
 	public String[] getRefineableColumns()
 	{
 		return new String[]{ColumnsWorkPlan.DATE.toString(), ColumnsWorkPlan.START.toString(), ColumnsWorkPlan.END.toString()};
-	}
-
-	@Override
-	public Class getColumnClass(int column)
-	{
-		switch (column)
-		{
-			case 0:
-				return Integer.class;
-
-			case 1:
-				return String.class;
-
-			case 2:
-				return TableDate.class;
-
-			case 3:
-				return String.class;
-
-			case 4:
-				return String.class;
-		}
-		return null;
 	}
 }
