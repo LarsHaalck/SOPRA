8a35b9c99c5e63cae8cc99d051e1176e73d6c38b
Florian Küpper <florian.kuepper@googlemail.com>
2015-03-11 11:34:20 +0100

    Save tour-edit to db now.

--- src/de/uni_muenster/sopra2015/gruppe8/octobus/controller/form/ControllerFormTourEdit.java
+++ src/de/uni_muenster/sopra2015/gruppe8/octobus/controller/form/ControllerFormTourEdit.java
@@ -104,12 +104,6 @@ public class ControllerFormTourEdit extends Controller implements ListenerButton
 		ArrayList<Bus> buses = controllerDatabase.getAvailableBusesForTour(tour);
 		ArrayList<Employee> employees = controllerDatabase.getAvailableBusDriversForTour(tour);
 
-		//TODO remove this
-		if(buses == null)
-			buses = new ArrayList<>();
-		if(employees == null)
-			employees = new ArrayList<>();
-
 		Object[][] data = new Object[buses.size()][2];
 		for (int i=0; i<buses.size(); i++)
 		{
