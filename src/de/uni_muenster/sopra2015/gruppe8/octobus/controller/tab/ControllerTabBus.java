d450033ceed2069f9e72608eccb520a45a137b96
Joshua <Joshua.Es@gmx.de>
2015-03-11 17:24:14 +0100

    TabSchedule refreshes if dependency is removed.

--- src/de/uni_muenster/sopra2015/gruppe8/octobus/controller/tab/ControllerTabBus.java
+++ src/de/uni_muenster/sopra2015/gruppe8/octobus/controller/tab/ControllerTabBus.java
@@ -37,6 +37,7 @@ public class ControllerTabBus extends Controller implements ListenerButton, List
 					{
 						controllerDatabase.deleteBus(tabBus.getSelectedID());
 						fillTable();
+						ControllerManager.informTableContentChanged(EmitterTable.TAB_SCHEDULE);
 					}
 				}
 				else
