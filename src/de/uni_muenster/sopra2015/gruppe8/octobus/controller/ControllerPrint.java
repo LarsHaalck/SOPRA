5d9ce36a4e386c0056873d265e5186c3d9b91777
Florian Küpper <florian.kuepper@googlemail.com>
2015-03-12 13:36:15 +0100

    Added @pres for print-methods.. #23

--- src/de/uni_muenster/sopra2015/gruppe8/octobus/controller/ControllerPrint.java
+++ src/de/uni_muenster/sopra2015/gruppe8/octobus/controller/ControllerPrint.java
@@ -36,35 +36,7 @@ public class ControllerPrint extends Controller implements ListenerPrint
 		{
 			//Print stopping-point-plans
 			case STOPPING_POINT:
-				Book bookStoppingPoints = new Book();
-				int firstPage = 0;
-
-				//Every given id is an unique stopping-point
-				for (Integer objectId : objectIds)
-				{
-					//Create print-data and add them to print-book
-					PrintStoppingPoint printStoppingPoint = new PrintStoppingPoint(controllerDatabase.getStoppingPointById(objectId));
-					PrintViewStoppingPoint printViewStoppingPoint = new PrintViewStoppingPoint(printStoppingPoint, firstPage);
-					bookStoppingPoints.append(printViewStoppingPoint, new PageFormat(), printStoppingPoint.getNumRoutes());
-					firstPage += printStoppingPoint.getNumRoutes();
-				}
-
-				PrinterJob job = PrinterJob.getPrinterJob();
-				job.setPageable(bookStoppingPoints);
-
-				//Show dialog to user, select printer
-				boolean doPrint = job.printDialog();
-				if (doPrint)
-				{
-					try
-					{
-						job.print();
-					} catch (Exception e)
-					{
-						System.out.println(e.getMessage());
-						System.out.println("Don't print");
-					}
-				}
+				printStoppingPoints(objectIds);
 				break;
 
 		}
@@ -76,49 +48,96 @@ public class ControllerPrint extends Controller implements ListenerPrint
 		switch (emitter)
 		{
 			case WORK_PLAN:
-				if(objectId <= 0)
-					break;
-
-				//Get tour and employee-data
-				ArrayList<Tour> tours = controllerDatabase.getToursForEmployeeId(objectId);
-				Employee employee = controllerDatabase.getEmployeeById(objectId);
-
-				String name = employee.getName()+", "+employee.getFirstName();
-				ArrayList<Quadruple<String, Date, Integer, String>> tourData = new ArrayList<>();
-
-				for (Tour tour : tours)
-				{
-					//Build strings for printing
-					String tourDesc = tour.getRoute().getName() + " (" + tour.getRoute().getStart().getName() + " - " + tour.getRoute().getEnd().getName()+")";
-					String busName = tour.getBus() == null ? "" : tour.getBus().getLicencePlate();
-					tourData.add(new Quadruple<>(tourDesc, tour.getStartTimestamp(), tour.getRoute().getDuration(), busName));
-				}
-
-				//Create new print-job
-				PrintWorkPlan printWorkPlan = new PrintWorkPlan(name, tourData);
-				//PrintViewWorkPlan is only for managing data to print
-				PrintViewWorkPlan printViewWorkPlan = new PrintViewWorkPlan(printWorkPlan);
-				Book bookWorkPlan = new Book();
-				bookWorkPlan.append(printViewWorkPlan, new PageFormat(), printViewWorkPlan.getNumberOfPages());
-
-
-				PrinterJob job = PrinterJob.getPrinterJob();
-				job.setPageable(bookWorkPlan);
-
-				//Show dialog to user, select printer
-				boolean doPrint = job.printDialog();
-				if(doPrint)
-				{
-					try{
-						job.print();
-					}
-					catch(Exception e)
-					{
-						System.out.println(e.getMessage());
-						System.out.println("Don't print");
-					}
-				}
+				printWorkPlan(objectId);
 				break;
+		}
+	}
+
+	/**
+	 * Prints work-plan for given user-id
+	 * @param userId
+	 * @pre user-id is valid id in employee-table in db, corresponding employee has Bus-Driver-Role
+	 * @post print-job was sent if everything was okay
+	 */
+	private void printWorkPlan(int userId)
+	{
+		//Get tour and employee-data
+		ArrayList<Tour> tours = controllerDatabase.getToursForEmployeeId(userId);
+		Employee employee = controllerDatabase.getEmployeeById(userId);
+
+		String name = employee.getName()+", "+employee.getFirstName();
+		ArrayList<Quadruple<String, Date, Integer, String>> tourData = new ArrayList<>();
+
+		for (Tour tour : tours)
+		{
+			//Build strings for printing
+			String tourDesc = tour.getRoute().getName() + " (" + tour.getRoute().getStart().getName() + " - " + tour.getRoute().getEnd().getName()+")";
+			String busName = tour.getBus() == null ? "" : tour.getBus().getLicencePlate();
+			tourData.add(new Quadruple<>(tourDesc, tour.getStartTimestamp(), tour.getRoute().getDuration(), busName));
+		}
+
+		//Create new print-job
+		PrintWorkPlan printWorkPlan = new PrintWorkPlan(name, tourData);
+		//PrintViewWorkPlan is only for managing data to print
+		PrintViewWorkPlan printViewWorkPlan = new PrintViewWorkPlan(printWorkPlan);
+		Book bookWorkPlan = new Book();
+		bookWorkPlan.append(printViewWorkPlan, new PageFormat(), printViewWorkPlan.getNumberOfPages());
+
+
+		PrinterJob job = PrinterJob.getPrinterJob();
+		job.setPageable(bookWorkPlan);
+
+		//Show dialog to user, select printer
+		boolean doPrint = job.printDialog();
+		if(doPrint)
+		{
+			try{
+				job.print();
+			}
+			catch(Exception e)
+			{
+				System.out.println(e.getMessage());
+				System.out.println("Don't print");
+			}
+		}
+	}
+
+	/**
+	 * Prints bus-stopping-point-plans
+	 * @param objectIds
+	 * @pre all ids in objectIds are valid ids in stopping-points-db-table.
+	 * @post print-job was sent if everything was okay
+	 */
+	private void printStoppingPoints(ArrayList<Integer> objectIds)
+	{
+		Book bookStoppingPoints = new Book();
+		int firstPage = 0;
+
+		//Every given id is an unique stopping-point
+		for (Integer objectId : objectIds)
+		{
+			//Create print-data and add them to print-book
+			PrintStoppingPoint printStoppingPoint = new PrintStoppingPoint(controllerDatabase.getStoppingPointById(objectId));
+			PrintViewStoppingPoint printViewStoppingPoint = new PrintViewStoppingPoint(printStoppingPoint, firstPage);
+			bookStoppingPoints.append(printViewStoppingPoint, new PageFormat(), printStoppingPoint.getNumRoutes());
+			firstPage += printStoppingPoint.getNumRoutes();
+		}
+
+		PrinterJob job = PrinterJob.getPrinterJob();
+		job.setPageable(bookStoppingPoints);
+
+		//Show dialog to user, select printer
+		boolean doPrint = job.printDialog();
+		if (doPrint)
+		{
+			try
+			{
+				job.print();
+			} catch (Exception e)
+			{
+				System.out.println(e.getMessage());
+				System.out.println("Don't print");
+			}
 		}
 	}
 
